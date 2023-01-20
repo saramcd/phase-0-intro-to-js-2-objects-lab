cc6191ba8f985ce4feb22db28ebc592a1959b662
Sara McDonnell <saramcdonnell32@gmail.com>
2023-01-20 14:23:31 -0500

    lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+const employee = {
+  name: "Mitty Ingate",
+  streetAddress: "25 Meow Mix Lane",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const employeeCopy = { ...employee };
+  delete employeeCopy[key];
+  return employeeCopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
